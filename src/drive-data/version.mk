ec46645f9e8db72f316d89b09a795c259f35f6e5
Trevor Cooper <tcooper@sdsc.edu>
2015-10-01 13:32:11 -0700

    Updates for roll configuration and build

--- src/drive-data/version.mk
+++ src/drive-data/version.mk
@@ -3,19 +3,13 @@ ifndef ROLLCOMPILER
 endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
-NAME           = sdsc-drive-data
-VERSION        = 1.0
-RELEASE        = 1
-PKGROOT        = /opt/drive-data
+NAME           = sdsc-seagate
+VERSION        = 6.2
+RELEASE        = 0
+PKGROOT        = /opt/seagate
 
-SRC_SUBDIR     = drive-data
+SRC_SUBDIR     = seagate
 
-SOURCE_NAME    = drive-data
-SOURCE_SUFFIX  = tgz
-SOURCE_VERSION = $(VERSION)
-SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
-
-TGZ_PKGS       = $(SOURCE_PKG)
+RPMS           = sdsc-seagate-private-6.2-0.x86_64.rpm
 
 RPM.EXTRAS     = AutoReq:No
