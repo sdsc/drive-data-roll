14550dab59bdfc7c5d54f69204e1af87cea7e49f
Trevor Cooper <tcooper@sdsc.edu>
2015-10-01 16:37:05 -0700

    Updates for readability and functionality...
    - Added variable for script install dir
    - Added function to create metrics path in /scratch/drive-data
    - Fixed awk matching with variable
    - Fixed storage of start scripts in array
    - Fixed parsing of start/stop script arrays for start and end functions

--- src/drive-data/seagate/stxnodemetricsmanager.sh
+++ src/drive-data/seagate/stxnodemetricsmanager.sh
@@ -11,7 +11,9 @@ version=0.5
 # The End function terminates any running time-based scripts and starts job-end scripts
 # Logs activity to log file
 
-if [ $# -ne 4 ];then #note can't log this as log is not set up at this point
+set -e
+
+if [ $# -ne 4 ]; then #note can't log this as log is not set up at this point
   echo "Please specify function[start|end] application-name job-id run-time on command line"
   exit 1
 fi
@@ -25,99 +27,132 @@ declare runtime=$4 #Runtime in seconds
 #Calculate time parameters for time based scripts
 declare -i runsecs=$runtime #run time in seconds, entire period over which the script runs
 declare -i tracedwell=$runsecs #duration of a single trace in seconds
-if [ $tracedwell -gt 1800 ];then tracedwell=1800;fi #1/2 hour max
+if [ $tracedwell -gt 1800 ]; then tracedwell=1800; fi #1/2 hour max
 declare -i sampleperiodsecs=$runsecs #duration of a period within which trace sample is taken
-if [ $sampleperiodsecs -gt 3600 ];then sampleperiodsecs=3600;fi #1 hour max
+if [ $sampleperiodsecs -gt 3600 ]; then sampleperiodsecs=3600; fi #1 hour max
 declare -i firstwaitsecs=-1 #delay in seconds to first trace sample, -1=no initial sample
 declare -i padsecs=0 #seconds from end of trace to end of sample period
 
 #Build folder name
 declare TESTdt=`date +%Y-%m-%d_%H-%M`
-declare filebase="$appname_jobid_`uname -n`"
-declare folder="results/$TESTdt_$filebase"
+declare filebase="$appname_jobid_$(hostname -s)"
+declare folder="/scratch/drive-data/$jobid/$TESTdt_$filebase"
 declare logpath="$folder/$filebase.log"
+
+#Detemine directory of executable
+declare stxappdir=$(dirname $0)
+
+logcreate()
+{
+    /bin/mkdir -p $folder
+    if [[ 0 -ne $? ]]; then
+        echo "Could not create $folder"
+        exit 1
+    else
+        /bin/touch $logpath
+        if [[ 0 -ne $? ]]; then
+            echo "Could not create $logpath"
+        exit 1
+    fi
+fi
+}
 
 logstart()
 {
-echo "Date-time stamp  Event version $version" >> $logpath
-logadd "Function $func, App $appname, Job $jobid, Runtime(sec) $runtime"
+    echo "Date-time stamp  Event version $version" >> $logpath
+    logadd "Function $func, App $appname, Job $jobid, Runtime(sec) $runtime"
 }
 
 logadd() #Event text passed as parameter
 {
-echo `date +%Y-%m-%d_%H-%M` $@ >> $logpath
+    echo `date +%Y-%m-%d_%H-%M` $@ >> $logpath
 }
 
+logcreate
 logstart
 
 #Discover drives as /dev/sdx and place in _drives array
 #Call SeaChest -s to get SCSI_generic names filtered by drive model
 #Expects the following format
 #SEAGATE   /dev/sg4  ST800FM0043             0056ed22               4.30      
+declare SEACHEST=${stxappdir}/SeaChest
+declare AWKBIN=$(which awk)
+declare ITDRVS=`$SEACHEST -s |grep "INTEL SSDSC2BB160G4R"   |tr -s ' '|cut -d" " -f2`
 declare SGDRVS=`$SEACHEST -s |grep "ST[0-9]00FM"   |tr -s ' '|cut -d" " -f2`
-declare -a _drives=($SGDRVS)
+declare -a _drives=($ITDRVS $SGDRVS)
 logadd "${#_drives[@]} drives found"
+
 #Convert SCSI generic list, /dev/sgx, to block device list, /dev/sdy
-for ((ndx=0; ndx < ${#_drives[@]}; ndx++));do
-  local drive=`echo ${_drives[$ndx]} | cut -d"/" -f3` #strip /dev/
+for ((ndx=0; ndx < ${#_drives[@]}; ndx++)); do
+  _drive=`echo ${_drives[$ndx]} | cut -d"/" -f3` #strip /dev/
   #Find sgx string in /sys/class/scsi_generic
   # lrwxrwxrwx. 1 root root 0 Jul 23 09:32 sg0 -> ../../devices/pci0000:00/0000:00:02.0/0000:02:00.0/host0/target0:2:0/0:2:0:0/scsi_generic/sg0
+  #
   #Collect SCSI target string - targetH:B:T
-  local SCSIt=`ls -ls /sys/class/scsi_generic | $AWKBIN -F/ '{if ($11 == '$drive') print $8}'`
+  _SCSIt=`ls -ls /sys/class/scsi_generic | $AWKBIN -v pat=$_drive -F/ '$0 ~ pat {print $8}'`
   #Find target string in /sys/dev/block
   # lrwxrwxrwx. 1 root root 0 Jul 23 09:37 8:0 -> ../../devices/pci0000:00/0000:00:02.0/0000:02:00.0/host0/target0:2:0/0:2:0:0/block/sda
+  #
   #Collect block device name - sdy, and replace sgx with /dev/sdy in _drives array
-  $_drives[$ndx]="/dev/"`ls -ls /sys/dev/block | $AWKBIN -F/ '{if ($8 == '$SCSIt') print $11}'`
-  logadd "/dev/$drive = ${_drives[$ndx]}"
+  _drives[$ndx]="/dev/"`ls -ls /sys/dev/block | $AWKBIN -v pat=$_SCSIt -F/ '$0 ~ pat {print $11}' | /bin/sort -u`
+  logadd "/dev/$_drive = ${_drives[$ndx]}"
 done
 
 #Define scripts and their parameters
 #Array for scripts to run at start. Can be added to. Run in their own thread.
-declare -a _scriptstostart=(\
-'./blktrscript.sh "'${_drives[@]}'" '"$runsecs $tracedwell $sampleperiodsecs $firstwaitsecs $padsecs $folder $filebase" \
-'./statsscript.sh "'${_drives[@]}'" '"$runsecs $tracedwell $sampleperiodsecs $firstwaitsecs $padsecs $folder $filebase")
+declare -a _scriptstostart=()
+for ((ndx=0; ndx < ${#_drives[@]}; ndx++)); do
+  script="'${stxappdir}/blktrscript.sh ${_drives[$ndx]} $runsecs $tracedwell $sampleperiodsecs $firstwaitsecs $padsecs $folder $filebase'"
+  _scriptstostart+=("$script")
+  script="'${stxappdir}/statsscript.sh ${_drives[$ndx]} $runsecs $tracedwell $sampleperiodsecs $firstwaitsecs $padsecs $folder $filebase'"
+  _scriptstostart+=("$script")
+done
+
 #Array for scripts to run at end. Can be added to. Run sequentially in this thread.
-declare -a _scriptsatend=("./stxm2.sh")
+declare -a _scriptsatend=("${stxappdir}/stxm2.sh")
 
 #Kick off sampling scripts in separate threads and return
-if [ $func == "START" ];then
+if [ $func == "START" ]; then
 
-  for script in ${_scriptstostart[@]};do
-    logadd "Starting $script"
-    $script &
-  done
-  exit 0
+    for ((ndx=0; ndx < ${#_scriptstostart[@]}; ndx++)); do
+        script=$(echo ${_scriptstostart[$ndx]} | /bin/sed "s/'//g") 
+        logadd "Starting ${script}"
+        ${script} &
+    done
+    exit 0
 fi
 
 #Kill sampling scripts if still running and kick off ending scripts
-if [ $func == "END" ];then
+if [ $func == "END" ]; then
 
-  #Kill any running sampling scripts
-  for script in ${_scriptstostart[@]};do
-    unset _commandparams
-    declare -a _commandparams=($script) #Break apart command and parameters into array so command is 1st element
-    declare command=${_commandparams[0]}
-    declare -i maxattempts=10 attempts=0
-    for (( attempts=0; attempts < maxattempts; attempts++ ));do
-      #ps -ef output format - UID        PID  PPID  C STIME TTY          TIME CMD
-      pid=`ps -ef | gawk '{if ($9 == "'$command'") print $2}'` #CMD will be /bin/bash, so command is $9
-      if [ "X$pid" != "X" ]; then kill -SIGUSR1 $pid;elif break;fi
-      sleep 1
+    #Kill any running sampling scripts
+    for ((ndx=0; ndx < ${#_scriptstostart[@]}; ndx++)); do
+        unset _commandparams
+        script=$(echo ${_scriptstostart[$ndx]} | /bin/sed "s/'//g")
+        declare -a _commandparams=(${script}) #Break apart command and parameters into array so command is 1st element
+        declare command=${_commandparams[0]}
+        declare -i maxattempts=10 attempts=0
+        for (( attempts=0; attempts < maxattempts; attempts++ )); do
+            #ps -ef output format - UID        PID  PPID  C STIME TTY          TIME CMD
+            pid=`ps -ef | gawk '{if ($9 == "'$command'") print $2}'` #CMD will be /bin/bash, so command is $9
+            if [ "X$pid" != "X" ]; then kill -SIGUSR1 $pid; else break; fi
+            sleep 1
+        done
+        if [ $attempts -ge $maxattempts ]; then
+            logadd "Unable to stop $command in $maxattempts seconds"
+        else
+            logadd "Stopped $command in $attempts seconds"
+        fi
     done
-    if [ $attempts -ge $maxattempts ]; then
-      logadd "Unable to stop $command in $maxattempts seconds"
-    else
-      logadd "Stopped $command in $attempts seconds"
-    fi
-  done
 
-  #Run end scripts
-  for script in ${_scriptsatend[@]};do
-    logadd "Running   $script"
-    $script
-    logadd "Completed $script"
-  done
-  exit 0
+    #Run end scripts
+    for ((ndx=0; ndx < ${#_scriptsatend[@]}; ndx++)); do
+        script=$(echo ${_scriptstostart[$ndx]} | /bin/sed "s/'//g")
+        logadd "Running   $script"
+        ${script}
+        logadd "Completed $script"
+    done
+    exit 0
 fi
 
 msg="Invalid function $func"
